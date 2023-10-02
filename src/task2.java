217ea7951705aec1c22ea470a86656dc87a35e64
yaroslavgorshkov <neznajkinarik@gmail.com>
2023-10-02 11:54:36 +0300

    add hw2

---
+++ src/task2.java
@@ -0,0 +1,20 @@
+public class task2 {
+    public static void main(String[] args) {
+        int number = 122212123;
+        System.out.println("number = " + number);
+        boolean result = isEven(number);
+        if (result)
+        {
+            System.out.println("number - четное число");
+        }
+        else
+        {
+            System.out.println("number - нечетное число");
+        }
+    }
+
+    public static boolean isEven (int number)
+    {
+        return number%2==0;
+    }
+}
