74d695f38d811c90a7bfb5e7fe47c5b3bdddee24
yaroslavgorshkov <neznajkinarik@gmail.com>
2023-10-16 10:12:44 +0300

    add hw6

---
+++ src/ua/gorshkov/hw6/Task2.java
@@ -0,0 +1,32 @@
+package ua.gorshkov.hw6;
+
+/*
+Ошибка на строке №23, ArrayIndexOutOfBoundsException. На строке 22 вместо i < triangle[i].length нужно
+писать j < triangle[i].length. Иначе программа на следующей строке подставляет triangle[0][1], хотя такого элемента нет.
+*/
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[][] triangle = new int[5][];
+        triangle[0] = new int[1];
+        triangle[1] = new int[2];
+        triangle[2] = new int[3];
+        triangle[3] = new int[4];
+        triangle[4] = new int[5];
+        for (int i = 0; i < triangle.length; i++) {
+            System.out.println(triangle.length + "!");
+            System.out.println("i = " + i);
+            for (int j = 0; j < triangle[i].length; j++) {
+                System.out.println(triangle[i].length);
+                triangle[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                System.out.print(triangle[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
