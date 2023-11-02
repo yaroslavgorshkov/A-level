d6a519545e283a81b128d255178697ebdb221b0a
yaroslavgorshkov <neznajkinarik@gmail.com>
2023-11-02 12:19:16 +0200

    add new hw9

---
+++ src/ua/gorshkov/hw9/Animal.java
@@ -0,0 +1,30 @@
+package ua.gorshkov.hw9;
+
+public class Animal {
+    private String kindOfFood;
+    private int hoursOfSleeping;
+
+    public String getKindOfFood() {
+        return kindOfFood;
+    }
+
+    public int getHoursOfSleeping() {
+        return hoursOfSleeping;
+    }
+
+    public Animal(String kindOfFood, int hoursOfSleeping) {
+        this.kindOfFood = kindOfFood;
+        this.hoursOfSleeping = hoursOfSleeping;
+    }
+
+    public Animal() {
+    }
+
+    public void eat(){
+        System.out.println("Eat from animal " + kindOfFood);
+    }
+
+    public final void sleep(){
+        System.out.println("Sleep from animal " + hoursOfSleeping);
+    }
+}
---
+++ src/ua/gorshkov/hw9/Main.java
@@ -0,0 +1,21 @@
+package ua.gorshkov.hw9;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Animal("meat", 10);
+        Animal cat = new Animal("mouse", 12);
+        Animal bee = new Animal("honey", 1);
+        int biggestTimeOfSleeping = getBiggestSleepingTime(dog, cat, bee);
+        System.out.println("Biggest sleeping time = " + biggestTimeOfSleeping);
+    }
+    public static int getBiggestSleepingTime(Animal first, Animal second, Animal third) {
+        int firstHoursOfSleeping = first.getHoursOfSleeping();
+        int secondHoursOfSleeping = second.getHoursOfSleeping();
+        int thirdHoursOfSleeping = third.getHoursOfSleeping();
+
+        return (firstHoursOfSleeping>=secondHoursOfSleeping
+                && firstHoursOfSleeping>=thirdHoursOfSleeping )
+                ? firstHoursOfSleeping : Math.max(secondHoursOfSleeping, thirdHoursOfSleeping);
+    }
+
+}
