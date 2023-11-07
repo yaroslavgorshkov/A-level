fba9de2b31626c6921c2d44e9c6c948c3c96528c
yaroslavgorshkov <neznajkinarik@gmail.com>
2023-11-07 17:15:56 +0200

    add changed hw9

--- src/ua/gorshkov/hw8/Main.java
+++ src/ua/gorshkov/hw8/Main.java
@@ -5,16 +5,23 @@ public class Main {
         Animal dog = new Animal("meat", 10);
         Animal cat = new Animal("mouse", 12);
         Animal bee = new Animal("honey", 1);
-        String biggestTimeOfSleepingAnimalsFood = getBiggestSleepingTimeAnimalsFood(dog, cat, bee);
+
+        Animal[] amimals = new Animal[3];
+        amimals[0] = dog;
+        amimals[1] = cat;
+        amimals[2] = bee;
+
+        String biggestTimeOfSleepingAnimalsFood = getBiggestSleepingTimeAnimalsFood(amimals);
         System.out.println("Biggest sleeping time animal`s food = " + biggestTimeOfSleepingAnimalsFood);
     }
 
-    private static String getBiggestSleepingTimeAnimalsFood(Animal first, Animal second, Animal third) {
-        int firstHoursOfSleeping = first.getHoursOfSleeping();
-        int secondHoursOfSleeping = second.getHoursOfSleeping();
-        int thirdHoursOfSleeping = third.getHoursOfSleeping();
-        return firstHoursOfSleeping >= secondHoursOfSleeping && firstHoursOfSleeping >= thirdHoursOfSleeping
-                ? first.getKindOfFood() : (secondHoursOfSleeping >= thirdHoursOfSleeping
-                ? second.getKindOfFood() : third.getKindOfFood());
+    private static String getBiggestSleepingTimeAnimalsFood(Animal[] amimals) {
+        Animal biggestSleepingTimeAnimal = amimals[0];
+        for(int i = 1; i < amimals.length; i ++) {
+            if(amimals[i].getHoursOfSleeping()>biggestSleepingTimeAnimal.getHoursOfSleeping()){
+                biggestSleepingTimeAnimal = amimals[i];
+            }
+        }
+        return biggestSleepingTimeAnimal.getKindOfFood();
     }
 }
