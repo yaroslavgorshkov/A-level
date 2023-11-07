2c51cd05aeea5de720dc8cee1ac73f3d4bd8b98c
yaroslavgorshkov <neznajkinarik@gmail.com>
2023-11-07 17:33:38 +0200

    add hw10

---
+++ src/ua/gorshkov/hw9/InvalidValueException.java
@@ -0,0 +1,7 @@
+package ua.gorshkov.hw9;
+
+public class InvalidValueException extends RuntimeException{
+    public InvalidValueException(String message) {
+        super(message);
+    }
+}
--- src/ua/gorshkov/hw9/Square.java
+++ src/ua/gorshkov/hw9/Square.java
@@ -4,6 +4,9 @@ public class Square extends Figure{
     private double sideLength;
 
     public Square(double sideLength) {
+        if (sideLength < 0) {
+            throw new InvalidValueException("Side length cannot be negative!");
+        }
         this.sideLength = sideLength;
     }
 
