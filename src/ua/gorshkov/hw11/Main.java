36d30a1e36663c8fa2baa2edd61a00c6a904dc17
yaroslavgorshkov <neznajkinarik@gmail.com>
2023-11-14 12:46:16 +0200

    add changed hw11

--- src/ua/gorshkov/hw11/Main.java
+++ src/ua/gorshkov/hw11/Main.java
@@ -1,4 +1,28 @@
 package ua.gorshkov.hw11;
 
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>(5);
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+
+        arrayList.add(6);
+
+        System.out.println(arrayList.remove(4));
+        System.out.println(arrayList.remove(4));
+        System.out.println(arrayList.get(3));
+        System.out.println(arrayList.get(3));
+
+        System.out.println(arrayList.isContains(3));
+
+        System.out.println(arrayList.indexOf(3));
+        System.out.println(arrayList.indexOf(30));
+
+        arrayList.add(6, 0);
+
+        System.out.println(arrayList.get(0));
+    }
 }
