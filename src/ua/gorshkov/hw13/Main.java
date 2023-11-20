a22b7777e23f7b6c6239f43bc0e8aa5ddb97f32c
yaroslavgorshkov <neznajkinarik@gmail.com>
2023-11-20 13:05:00 +0200

    add hw13

---
+++ src/ua/gorshkov/hw13/Main.java
@@ -0,0 +1,15 @@
+package ua.gorshkov.hw13;
+
+import java.util.*;
+import ua.gorshkov.hw12.Figure;
+import ua.gorshkov.hw12.Circle;
+import ua.gorshkov.hw12.Triangle;
+import ua.gorshkov.hw12.Square;
+
+import ua.gorshkov.hw12.InvalidValueException;
+import ua.gorshkov.hw12.ImpossibleTriangleException;
+public class Main {
+    public static void main(String[] args) {
+        Figure circle1 = new Circle(10);
+    }
+}
